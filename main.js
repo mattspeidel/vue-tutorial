bafa8c4c0f29f6b7817175e0eee360154a1d34ea
mattspeidel <mattspeidel@gmail.com>
2019-09-13 13:38:34 -0400

    through part 2 of vuemastery tut

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: 'mvaasfue1uwpcrty6nes.webp',
+        link: 'https://www.nike.com/t/everyday-plus-cushion-training-crew-socks-3-pairs-kt8drs'
     }
 })
