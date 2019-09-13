c41d4a7060fbcfcc1982673f5c69b5d39ea230b1
mattspeidel <mattspeidel@gmail.com>
2019-09-13 14:07:41 -0400

    through vuemastery tut part 5

--- main.js
+++ main.js
@@ -4,6 +4,29 @@ var app = new Vue({
         product: 'Socks',
         description: 'A pair of warm, fuzzy socks',
         image: 'mvaasfue1uwpcrty6nes.webp',
-        link: 'https://www.nike.com/t/everyday-plus-cushion-training-crew-socks-3-pairs-kt8drs'
+        link: 'https://www.nike.com/t/everyday-plus-cushion-training-crew-socks-3-pairs-kt8drs',
+        inventory: 8,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "gray",
+                variantImage: 'mvaasfue1uwpcrty6nes.webp',
+            },
+            {
+                variantId: 2235,
+                variantColor: "black",
+                variantImage: 'a8foncooah9f75mroq3p.webp',
+            }
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+    updateProduct(variantImage) {
+        this.image = variantImage
+    }
     }
 })
